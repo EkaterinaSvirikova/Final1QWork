db3701d4882667ed26b769fc2cd78a7102bd2080
Ekaterina <pechnikova.07@mail.ru>
2022-08-21 17:02:54 +0300

    Task 1var added

---
+++ Task/Program.cs
@@ -0,0 +1,39 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа.
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+ 
+using System;
+using static System.Console;
+Clear();
+ 
+string[] array = {"hello", "2", "world", ":-)"};
+
+WriteLine($"В исходном массиве [{String.Join(", ", array)}] количество строк, соответствующих условию = {GetCount(array)} это {FindStringsInArray1(array)}");
+WriteLine();
+
+int GetCount(string[] ar)
+{
+   int count = 0;
+   for(int i = 0; i < ar.Length; i++)
+   {
+       if(ar[i].Length <= 3)
+       {
+           count++;
+       }
+   }
+   return count;
+}
+string FindStringsInArray1(string[] arrStr)
+{
+   string result = String.Empty;
+   for(int i = 0; i < arrStr.Length; i++)
+   {
+       if(arrStr[i].Length <= 3)
+       {
+           result += $"{arrStr[i]} ";
+       }
+   }
+   return result;
+}
